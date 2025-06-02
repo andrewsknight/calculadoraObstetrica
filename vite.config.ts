5e691f0b331d32991c6c84a1495e4503eac6cdbb
Andrews Knight <andrescaballerojob@gmail.com>
2025-06-02 10:02:09 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/calculadoraObstetrica/',  // 👈 Añade esta línea (muy importante)
   plugins: [react()],
   resolve: {
     alias: {
